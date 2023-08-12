f26564d5915e63965ba1f59595504691b2857314
Ryolin <manilrabia@gmail.com>
2023-08-12 19:34:18 +0100

    about

---
+++ bdd.sql
@@ -0,0 +1,9 @@
+-- CREER LA TABLE
+CREATE TABLE services (
+    name TEXT NOT NULL,
+    price FLOAT NOT NULL,
+    description TEXT,
+);
+
+-- INSERER
+INSERT INTO services (name, price, description) VALUES ('', 0, '');
