a87fd6ae5f8992d0d9a7b0b574631cb5d4569db4
Ryolin <manilrabia@gmail.com>
2023-08-14 04:18:30 +0100

    drop-down menu est ouvert par tout le titre, pas que fleche

--- static/js/services.js
+++ static/js/services.js
@@ -1,22 +1,22 @@
 // start
 document.addEventListener('DOMContentLoaded', function() {
     // drop-down menu
-    // link content to its arrow
-    let arrows = document.querySelectorAll('.drop-down-svg-container');
-    arrows.forEach((arrow) => {
-        let content = arrow.parentElement.parentElement.children[1];
+    // link content to its menu
+    let menus = document.querySelectorAll('.drop-down-title-container');
+    menus.forEach((menu) => {
+        let content = menu.parentElement.children[1];
+        let arrow = menu.children[1].children[0];
         content.height = content.clientHeight;
         content.setAttribute('style', 'height: 0;');
-        arrow.content = content;
+        menu.content = content;
+        menu.arrow = arrow;
     });
 
-    // arrows
-    arrows.forEach((arrow) => {
-        // content
-        let content = arrow.content;
-        // svg
-        arrow = arrow.children[0];
-        arrow.addEventListener('click', function() {
+    // menus
+    menus.forEach((menu) => {
+        let content = menu.content;
+        let arrow = menu.arrow;
+        menu.addEventListener('click', function() {
             if (window.getComputedStyle(arrow).transform == 'matrix(-1, 0, 0, -1, 0, 0)')
             {
                 content.setAttribute('style', 'height: ' + content.height.toString() + 'px; margin-top: 20px;');
